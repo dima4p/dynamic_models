bad11ee7dd3549af3ef685696eb7bc3ca2633191
Dmitri Koulikoff (acer) <dima@koulikoff.ru>
2013-09-26 03:13:57 +0400

    Page risk_report is done 42.5h

--- app/assets/javascripts/static.js.coffee
+++ app/assets/javascripts/static.js.coffee
@@ -1,3 +1,11 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+# Place all the behaviors and hooks related to the StaticController here.
+
+$ ->
+  if $('body.static').length
+    $(document).on 'change', 'body.static.risk_report input[type=checkbox]', ->
+      label = $(this).prev()
+      label.removeClass 'on off'
+      if this.checked
+        label.addClass 'on'
+      else
+        label.addClass 'off'
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -15,6 +15,20 @@ class StaticController < ApplicationController
   def set_user_data
     @user_name = 'Dmitri Ivanov'
     @user_pic = 'ivanov.jpg'
+    @content = 
+      [{title: 'Mining project management, infrastructure and civil engineering',
+        content: [{title: 'Surface facilities',
+                   content: [
+                             'DRA Mining provides a total mining solution which includes: consulting services, exploration management, geology, studies, Competent Person reports, resource modeling, mine design, mine scheduling, surface and underground infrastructure, design and engineering, materials handling, construction management, operational support, maintenance and mine closure.',
+                             'This service, which covers the entire spectrum of industrial, civil and structural design and construction, is managed by a specialist engineering and construction team.'
+                    ]},
+                  {title: 'Others',
+                   content: 'This service, which covers the entire spectrum of industrial, civil and structural design and construction, is managed by a specialist engineering and construction team.'
+                 }]},
+        {title: 'Minerals processing plant design',
+         content: [{content: 'DRA Mineral Projects has been successfully involved in some of the most challenging mine and processing developments in the world. The company has the expertise to conceptualise and design mineral processing plants from run-of-mine ore to saleable product in the platinum, coal, diamond, gold, ferrous and base metals, uranium and phosphate sectors, amongst others.'}]},
+        {title: 'Project management for mineral processing plants',
+         content: [{content: %q(DRA’s project management capabilities ensure total control across all types of projects. By drawing on its collective expertise, the company is able to direct the entire project process from conceptual feasibility to final hand-over and beyond. DRA offers fast track implementation when required, testing and inspection, construction management and control as well as office and conference facilities for sites in remote areas.)}] }]
   end
 
 end
