5599af044104e67300c0c22d321021f53077960a
Dmitri Koulikoff (acer) <dima@koulikoff.ru>
2013-09-30 18:49:40 +0400

    Page "Inices of Efficiency" is added 56.7h

--- app/assets/javascripts/static.js.coffee
+++ app/assets/javascripts/static.js.coffee
@@ -1,10 +1,45 @@
 # Place all the behaviors and hooks related to the StaticController here.
 
-$ ->
-  $(document).on 'change', 'body.static.risk_report input[type=checkbox]', ->
-    label = $(this).prev()
-    label.removeClass 'on off'
-    if this.checked
-      label.addClass 'on'
-    else
-      label.addClass 'off'
+radius = 42
+canvas = 130
+center_x = canvas / 2
+center_y = canvas / 2 + 12
+
+arc = (value, total, color) ->
+  alpha = 360 / total * value
+  a = (90 - alpha) * Math.PI / 180
+  x = center_x + radius * Math.cos(a)
+  y = center_y - radius * Math.sin(a)
+  if total == value
+    path = [["M", center_x, center_y - radius]
+            ["A", radius, radius, 0, 1, 1, center_x - 0.01, center_y - radius]]
+  else
+    path = [["M", center_x, center_y - radius]
+            ["A", radius, radius, 0, +(alpha > 180), 1, x, y]]
+  {
+    path: path
+    stroke: color
+  }
+draw_arcs = ->
+  if $('body.static.efficiency').length
+    graphs = $('.graph')
+    color = graphs.css('color')
+    param = {"stroke-width": 10}
+    graphs.each ->
+      r = Raphael(this, canvas, canvas)
+      r.customAttributes.arc = arc
+      r.path().attr(param).attr({arc: [10, 10, color]})
+      if val = parseInt $(this).find('.number').html()
+        total = parseInt $(this).data('maximum')
+        c = $(this).css 'color'
+        r.path().attr(param).attr({arc: [val, total, c]})
+
+$ draw_arcs
+$(document).on 'page:load', draw_arcs
+$(document).on 'change', 'body.static.risk_report input[type=checkbox]', ->
+  label = $(this).prev()
+  label.removeClass 'on off'
+  if this.checked
+    label.addClass 'on'
+  else
+    label.addClass 'off'
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -5,16 +5,6 @@ class StaticController < ApplicationController
   before_filter :set_user_data
 
   def risk_report
-  end
-
-  def efficiency
-  end
-
-  private
-
-  def set_user_data
-    @user_name = 'Dmitri Ivanov'
-    @user_pic = 'ivanov.jpg'
     @content = 
       [{title: 'Mining project management, infrastructure and civil engineering',
         content: [{title: 'Surface facilities',
@@ -29,6 +19,25 @@ class StaticController < ApplicationController
          content: [{content: 'DRA Mineral Projects has been successfully involved in some of the most challenging mine and processing developments in the world. The company has the expertise to conceptualise and design mineral processing plants from run-of-mine ore to saleable product in the platinum, coal, diamond, gold, ferrous and base metals, uranium and phosphate sectors, amongst others.'}]},
         {title: 'Project management for mineral processing plants',
          content: [{content: %q(DRA’s project management capabilities ensure total control across all types of projects. By drawing on its collective expertise, the company is able to direct the entire project process from conceptual feasibility to final hand-over and beyond. DRA offers fast track implementation when required, testing and inspection, construction management and control as well as office and conference facilities for sites in remote areas.)}] }]
+  end
+
+  def efficiency
+    @indices = [
+      ['accidents', 0, 0, 0, 1],
+      ['emergencies', 0, 0, 0, 1],
+      ['violations', 0, 5, 10, 17]
+    ]
+    @throughput_params = [
+      ['value_of_realization', 13, 5, 12, 100],
+      ['value_of_realization', 594, 575, 558, 800]
+     ]
+  end
+
+  private
+
+  def set_user_data
+    @user_name = 'Dmitri Ivanov'
+    @user_pic = 'ivanov.jpg'
   end
 
 end
